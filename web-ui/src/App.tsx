804b84ad63bb97d39b847948d8e27cfa38f2e3f4
anniebhalla16 <anniebhalla16@gmail.com>
2025-08-12 17:33:03 +0200

    fix: ui for chatbox

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,15 +1,15 @@
 import { Provider } from 'react-redux';
 import './App.css';
-import { Counter } from './components/counter';
+import Chatbox from './components/Chatbox';
 import { store } from './redux-store/store';
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <div className="h-full w-full bg-gradient-to-br from-slate-100 to-slate-200">
-          {/* <Chatbox /> */}
-          <Counter />
+        <div className="h-full w-ful">
+          <Chatbox />
+          {/* <Counter /> */}
         </div>
       </Provider>
     </>
